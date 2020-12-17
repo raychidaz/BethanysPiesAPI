94ab2f739a1420bdde2a39bbd3016a0687b1b97b
Ray Chidaz <raychidaz@gmail.com>
2020-12-17 22:41:11 +0000

    built REST API methods to modify data (POST, PUT, DELETE and PATCH), return 201 after succesful post, return 404 if record is not found for updating, patching or deleting, return 200 or 204 from a delete in JSON envelope

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Bring in the express server and create an application
 let express = require('express');
 let app = express();
-
 let pieRepo = require('./repos/pieRepo');
 
 // Use the express Router object
 let router = express.Router();
+
+// Configure middleware to support JSON data parsing in request object
+app.use(express.json());
 
 // Create GET to return a list of all pies
 router.get('/', (request, response, next) => {
@@ -77,6 +79,106 @@ router.get('/:id', function (req, res, next) {
   );
 });
 
+// Create POST endpoint to inert data
+router.post('/', function (req, res, next) {
+  pieRepo.insert(
+    req.body,
+    (data) => {
+      res.status(201).json({
+        status: 201,
+        statusText: 'Created',
+        message: 'New pie added',
+        data,
+      });
+    },
+    function (err) {
+      next(err);
+    },
+  );
+});
+
+// Create PUT endpoint to update data
+router.put('/:id', function (req, res, next) {
+  pieRepo.getById(
+    req.params.id,
+    (data) => {
+      if (data) {
+        // Attempt to updatae data
+        pieRepo.update(req.body, req.params.id, (data) => {
+          res.status(200).json({
+            status: 200,
+            statusText: 'OK',
+            message: `Pie ${req.params.id} updated`,
+            data: data,
+          });
+        });
+      } else {
+        res.status(404).json({
+          status: 404,
+          statusText: 'Not Found',
+          message: `The pie ${req.params.id} could not be found`,
+          error: {
+            code: 'NOT FOUND',
+            message: `The pie ${req.params.id} could not be found`,
+          },
+        });
+      }
+    },
+    function (err) {
+      next(err);
+    },
+  );
+});
+
+// Create DELETE endpoint to delete data
+router.delete('/:id', function (req, res, nex) {
+  pieRepo.getById(
+    req.params.id,
+    (data) => {
+      if (data) {
+        // Attempt to delete the data
+        pieRepo.delete(req.params.id, (data) => {
+          res.status(200).json({
+            status: 200,
+            statusText: 'OK',
+            message: `The pie at ${req.params.id} is deleted`,
+            data: `Pie ${req.params.id} deleted`,
+          });
+        });
+      } else {
+        res.status(404).json({
+          status: 404,
+          statusText: 'Not Found',
+          message: `The pie ${req.params.id} could not be found`,
+          error: {
+            code: 'NOT FOUND',
+            message: `The pie ${req.params.id} could not be found`,
+          },
+        });
+      }
+    },
+    function (err) {
+      next(err);
+    },
+  );
+});
+
+// Create PATCH endpoint to Patch data - call update with partial data
+router.patch('/:id', function (req, res, next) {
+  pieRepo.getById(req.params.id, (data) => {
+    if (data) {
+      // Attempt to update data
+      pieRepo.update(req.body, req.params.id, (data) => {
+        res.status(200).json({
+          status: 200,
+          statusText: 'OK',
+          message: `Pie ${req.params.id} patched`,
+          data: data,
+        });
+      });
+    }
+  });
+});
 // Configure router so all routes are prefixed with  /api/v1
 app.use('/api/', router);
 
--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -49,6 +49,64 @@ let pieRepo = {
       }
     });
   },
+  insert: function (newData, resolve, reject) {
+    fs.readFile(FILE_NAME, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let pies = JSON.parse(data);
+        pies.push(newData);
+        fs.writeFile(FILE_NAME, JSON.stringify(pies), (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(newData);
+          }
+        });
+      }
+    });
+  },
+  update: function (newData, id, resolve, reject) {
+    fs.readFile(FILE_NAME, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let pies = JSON.parse(data);
+        let pie = pies.find((pie) => pie.id == id);
+        if (pie) {
+          Object.assign(pie, newData);
+          fs.writeFile(FILE_NAME, JSON.stringify(pies), (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(newData);
+            }
+          });
+        }
+      }
+    });
+  },
+
+  delete: function (id, resolve, reject) {
+    fs.readFile(FILE_NAME, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let pies = JSON.parse(data);
+        let index = pies.findIndex((pie) => pie.id == id);
+        if (index != -1) {
+          pies.splice(index, 1);
+          fs.writeFile(FILE_NAME, JSON.stringify(pies), (err) => {
+            if (err) {
+              reject;
+            } else {
+              resolve(index);
+            }
+          });
+        }
+      }
+    });
+  },
 };
 
 module.exports = pieRepo;
